94856b7d17736f7cc69c69549d95a1cb648474c8
Piotr Kalwig <p.kalwig88@gmail.com>
2019-02-16 11:27:02 +0100

    Fix nested listviews

--- lib/widgets/topology_versions_widget.dart
+++ lib/widgets/topology_versions_widget.dart
@@ -14,6 +14,7 @@ class TopologyVersionsWidget extends StatelessWidget {
       itemCount: _topologyVersionsDTO.topologies.length,
       itemBuilder: (context, index) =>
           TopologyWidget(_topologyVersionsDTO.topologies[index]),
+      shrinkWrap: true,
     );
   }
 }

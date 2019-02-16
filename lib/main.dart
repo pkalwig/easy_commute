6fb5c3a06bdaf6e4e43675e7142c89c6b1f7812d
Piotr Kalwig <p.kalwig88@gmail.com>
2019-02-16 11:29:36 +0100

    Add icon to FAB

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class AgenciesViewState extends State<AgenciesView> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => setState(() => _body = buildPost()),
+        child: Icon(Icons.cloud_download),
       ),
       body: _body,
     );

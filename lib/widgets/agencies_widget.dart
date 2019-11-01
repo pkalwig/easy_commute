cce7c8f56df6fea8765c583f002bf8358ec7b865
Piotr Kalwig <p.kalwig88@gmail.com>
2019-11-01 23:16:14 +0100

    Add separator between agencies

--- lib/widgets/agencies_widget.dart
+++ lib/widgets/agencies_widget.dart
@@ -10,10 +10,12 @@ class AgenciesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
       itemCount: _agenciesDto.agencies.length,
       itemBuilder: ((context, index) =>
           AgencyWidget(_agenciesDto.agencies[index])),
+      separatorBuilder: (context, index) => Divider(),
+
     );
   }
 }

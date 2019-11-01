160bcf5436e82e5958c1e6e6c9c60edc2e4c920f
Piotr Kalwig <p.kalwig88@gmail.com>
2019-11-01 23:17:16 +0100

    Decode agencies from HTTP request with UTF-8

--- lib/data_fetcher.dart
+++ lib/data_fetcher.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
@@ -8,7 +9,8 @@ class DataFetcher {
     if (response.statusCode != 200) {
       throw NetworkException();
     }
-    return response.body;
+    String body = utf8.decode(response.bodyBytes);
+    return body;
   }
 }
 

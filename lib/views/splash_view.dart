6ae18fb373336649936b8c45c8dbdb9ba8f1f33f
Piotr Kalwig <p.kalwig88@gmail.com>
2020-01-09 12:13:50 +0100

    Select agency on app first run

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
 
 import 'package:easy_commute/services/settings_service.dart';
+import 'package:easy_commute/views/home_view.dart';
+import 'package:easy_commute/views/welcome_view.dart';
 
 class SplashView extends StatelessWidget{
+  final initTask = SettingsService().isFirstRun;
+
   @override
-  Widget build(BuildContext context) =>
-    Scaffold(
-      body: SafeArea(
-        child: Center(
-            child: FutureBuilder<bool>(
-              future: SettingsService().isFirstRun(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data.toString());
+  Widget build(BuildContext context) => Scaffold(
+        body: SafeArea(
+          child: Center(
+              child: FutureBuilder<bool>(
+            future: initTask(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) =>
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => WelcomeView())));
+                } else {
+                  WidgetsBinding.instance.addPostFrameCallback((_) =>
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomeView())));
                 }
-                else if (snapshot.hasError) {
-                  return Center(child: Text("${snapshot.error}"));
-                }
-                return Center(child: CircularProgressIndicator());
-              },
-            )
+                return Text(snapshot.data.toString());
+              } else if (snapshot.hasError) {
+                return Center(child: Text("${snapshot.error}"));
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          )),
         ),
-      ),
-    );
+      );
 }

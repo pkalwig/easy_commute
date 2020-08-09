9fe2fc49132f373bb234b1b96d7cee118518afdf
Piotr Kalwig <p.kalwig88@gmail.com>
2020-08-09 19:27:24 +0200

    Add option to fetch line stop times

--- lib/dto_fetcher.dart
+++ lib/dto_fetcher.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:easy_commute/originDTO/origin_agencies_dto.dart';
 import 'package:easy_commute/data_fetch_urls.dart';
 import 'package:easy_commute/data_fetcher.dart';
+import 'package:easy_commute/originDTO/origin_line_stop_times_dto.dart';
 import 'package:easy_commute/originDTO/origin_scheduled_routes_dto.dart';
 
 class DtoFetcher {
@@ -23,6 +24,17 @@ class DtoFetcher {
     try {
       String fetchResult = await _dataFetcher.fetchPost(DataFetchUrl.scheaduledRoutes);
       result = OriginScheduledRoutesDTO.fromJson(json.decode(fetchResult));
+    } finally {}
+    return result;
+  }
+
+  Future<OriginLineStopTimesDTO> fetchLineStopTimes(
+      DateTime dateTime, int routeId) async {
+    OriginLineStopTimesDTO result;
+    try {
+      String fetchResult = await _dataFetcher.fetchPost(
+          'http://ckan2.multimediagdansk.pl/stopTimes?date=${dateTime.year}-${dateTime.month}-${dateTime.day}&routeId=$routeId');
+      result = OriginLineStopTimesDTO.fromJson(json.decode(fetchResult));
     } finally {}
     return result;
   }

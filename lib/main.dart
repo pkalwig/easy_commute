f4faa3b96f616c186516ede0eee7e4f22eb27b73
Piotr Kalwig <p.kalwig88@gmail.com>
2021-07-03 18:12:11 +0200

    Provide dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,12 @@ import 'package:easy_commute/views/splash_view.dart';
 
 void main() => runApp(MyApp());
 
-final ThemeData _myAppTheme = _buildMyAppTheme();
-
-ThemeData _buildMyAppTheme() => ThemeData.light();
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
       title: 'Easy Commute',
       home: SplashView(),
-      theme: _myAppTheme
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark()
   );
 }
